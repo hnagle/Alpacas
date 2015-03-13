9da7dec5bc0bf4293b0b5162e522354e6192e8b7
hnagle <harris.nagle1@marist.edu>
2015-03-13 09:53:15 -0400

    Lab #4
    Part 4 of Queries

--- Lab#4/Lab#4.sql
+++ Lab#4/Lab#4.sql
@@ -1,76 +1,81 @@
 Harris Nagle
 Lab#4
-Double Queries
+The Subqueries Sequel 
+ALPACAS
 
 
 
-1)
-select city
-from agents
-where agents.aid in (select aid
-		from orders
-		where orders.cid = 'c006');
+1) Get the cities of agents booking an order for a customer whose cid is c006
+
+SELECT city
+	FROM agents
+	WHERE agents.aid IN (SELECT aid
+		FROM orders
+		WHERE orders.cid = 'c006');
 
 
-2)
-select pid
-from orders
-where orders.aid in (
-		select aid
-		from orders
-		where orders.cid in (
-					select cid
-					from customers
-					where city = 'Kyoto'));
+2) Get the pids of products ordered through any agent who takes at least one  order from
+-- a customer in Kyoto, sorted pid from highest to lowest
 
-3)
-select name, cid
-from customers
-where customers.cid in (
-		select cid
-		from orders
-		where aid <> 'a03');
+SELECT pid
+	FROM orders
+	WHERE orders.aid IN (
+		SELECT aid
+		FROM orders
+		WHERE orders.cid IN (
+				SELECT cid
+				FROM customers
+				WHERE city = 'Kyoto'));
 
-4)
-select cid
-from orders
-where pid = 'p01'
-or pid = 'p07'
+3) Get the cids and names of customers who did not place an order through agent a03.
 
-5)
-select pid
-from orders
-where orders.cid NOT in (
-		select cid
-		from orders
-		where aid = 'a05');
+SELECT name, cid
+	FROM customers
+	WHERE customers.cid in (
+		SELECT cid
+		FROM orders
+		WHERE aid <> 'a03');
 
-6)
-select name, discount, city
-from customers
-where customers.cid in (
-		select cid
-		from orders
-		where orders.aid in ( 
-					select aid
-					from agents
-					where city = 'Dallas'
-					or city = 'New York'));
+4) Get the cids of customers who ordered both product p01 and p07.
 
-7)
-select name
-from customers
-where customers.discount in (
-			select discount
-			from customers
-			where city = 'Dallas'
-			or city = 'London')
+SELECT cid
+	FROM orders
+	WHERE pid = 'p01'
+		OR pid = 'p07'
+
+5) Get the pids of products NOT ordered by any customers who placed any order through agent a05.
+
+SELECT pid
+	FROM orders
+	WHERE orders.cid NOT IN (
+		SELECT cid
+		FROM orders
+		WHERE aid = 'a05');
+
+6) Get the name, discounts, and city for all customers who place orders through agents in Dallas or New York.
+
+SELECT name, discount, city
+	FROM customers
+	WHERE customers.cid in (
+		SELECT cid
+		FROM orders
+		WHERE orders.aid IN ( 
+				SELECT aid
+				FROM agents
+				WHERE city = 'Dallas'
+				OR city = 'New York'));
+
+7) Get all customers who have the same discount as that of any customers in Dallas or London
+
+SELECT name
+	FROM customers
+	WHERE customers.discount IN (
+		SELECT discount
+		FROM customers
+		WHERE city = 'Dallas'			OR city = 'London')
 
 
-8) Constraints are used in database to describe limitations on what exactly data can be. They are valuable because they help
-organize data and make it easier to be analyzed later on. They make it easier later on because they support tables and their data 
-with the check constraints. A possible example of a check constraint is as follows.
-
-select * 
-	from orders
-	where dollars <1000
+8) Tell me about check constraints: What are they? What are they good for? What’s the advantage of 
+-- putting that sort of thing inside the database? Make up some examples of good uses of check 
+-- constraints and some examples of bad uses of check constraints. Explain the differences in 
+-- your examples and argue your case.
