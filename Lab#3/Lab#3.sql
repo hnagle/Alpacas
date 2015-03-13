002d82ca88858bfe516d19b31976d7c6c346a5a3
hnagle <harris.nagle1@marist.edu>
2015-03-13 09:55:08 -0400

    Lab #3
    Basic Queries

--- Lab#3/Lab#3.sql
+++ Lab#3/Lab#3.sql
@@ -1,42 +1,60 @@
 Harris Nagle
 Lab#3 
+ALPACAS
 
+1) List the ordno and dollars of all orders.
 
 SELECT ordno, dollars
-from orders;
+	FROM orders;
+
+2) List the name and city of agents named Smith
 
 SELECT name, city
-FROM agents
-WHERE name = 'Smith';
+	FROM agents
+	WHERE name = 'Smith';
+
+3) List the pid, name, and priceUSD of products with quantity more than 200,000
 
 SELECT pid, name, priceUSD
-FROM products
-WHERE quantity > 200000
+	FROM products
+	WHERE quantity > 200000
+
+4) List the names and cities of customers in Dallas
 
 SELECT name, city
-FROM customers
-WHERE city = 'Dallas';
+	FROM customers
+	WHERE city = 'Dallas';
+
+5) List the names of agents not in New york and not in TOkyo.
 
 SELECT name
-FROM agents
-WHERE city <> 'New York'
-OR city <> 'Tokyo';
+	FROM agents
+	WHERE city <> 'New York'
+	OR city <> 'Tokyo';
+
+6) List all data for products not in Dallas or Duluth that cost Us $1 or more
 
 SELECT pid, name, city, quantity, priceUSD
-FROM products
-WHERE priceUSD >= 1
-AND city <> 'Dallas'
-AND city <> 'Duluth';
+	FROM products
+	WHERE priceUSD >= 1
+		AND city <> 'Dallas'
+		AND city <> 'Duluth';
+
+7) List all data for orders in January or May
 
 SELECT ordno, mon, cid, aid, pid, qty, dollars
-FROM orders
-WHERE mon = 'jan'
-OR mon = 'may';
+	FROM orders
+	WHERE mon = 'jan'
+		OR mon = 'may';
+
+8) List all data for orders in February more than US $500
 
 SELECT ordno, mon, cid, aid, pid, qty, dollars
-FROM orders
-WHERE dollars > 500;
+	FROM orders
+	WHERE dollars > 500;
+
+9) List all orders from the customer whose cid is c005.
 
 SELECT
-FROM customers
-WHERE cid = 'c005';
+	FROM customers
+	WHERE cid = 'c005';
